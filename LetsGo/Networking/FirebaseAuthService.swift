06a1db7102c9d29579871fab7ea48e1de8c3dd6d
Vishal Polepalli <vishal.polepalli@gmail.com>
2022-10-07 10:50:43 -0500

    Add functions to get user details (signedIn, IDToken, signOut, currentUser)

--- LetsGo/Networking/FirebaseAuthService.swift
+++ LetsGo/Networking/FirebaseAuthService.swift
@@ -11,6 +11,10 @@ import FirebaseAuth
 protocol FirebaseAuthServiceProtocol: AnyObject {
     func requestOTP(phoneNumber: String, completion: @escaping (String?, Error?) -> Void)
     func signInWithCode(code: String, verificationId: String, completion: @escaping (AuthDataResult?, Error?) -> Void)
+    func getUserIDToken(completion: @escaping (AuthTokenResult?) -> Void)
+    func getCurrentUser() -> User?
+    func isUserSignedIn() -> Bool
+    func signOutUser()
 }
 
 class FirebaseAuthService: DependencyContainer.Component, FirebaseAuthServiceProtocol {
@@ -28,6 +32,33 @@ class FirebaseAuthService: DependencyContainer.Component, FirebaseAuthServicePro
 
         Auth.auth().signIn(with: credential) { (authResult, error) in
             completion(authResult,error)
+        }
+    }
+    
+    func getUserIDToken(completion: @escaping (AuthTokenResult?) -> Void) {
+        guard let currentUser = getCurrentUser() else {
+            completion(nil)
+            return
+        }
+        
+        currentUser.getIDTokenResult(forcingRefresh: true) { idToken, error in
+            completion(idToken)
+        }
+    }
+    
+    func getCurrentUser() -> User? {
+        return Auth.auth().currentUser
+    }
+    
+    func isUserSignedIn() -> Bool {
+        return getCurrentUser() != nil
+    }
+    
+    func signOutUser() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("Error: ", error)
         }
     }
 }
