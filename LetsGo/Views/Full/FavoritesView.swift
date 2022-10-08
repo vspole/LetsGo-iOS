0d3a51fb5e5b5c1000f8a6c66e54c1d9290d4c64
Vishal Polepalli <vishal.polepalli@gmail.com>
2022-10-07 23:02:42 -0500

    Search bar on favorites

--- LetsGo/Views/Full/FavoritesView.swift
+++ LetsGo/Views/Full/FavoritesView.swift
@@ -7,12 +7,18 @@
 
 
 import SwiftUI
+import Foundation
 
 struct FavoritesView: View {
     @StateObject var viewModel: ViewModel
 
     var body: some View {
         VStack {
+            searchBar
+                .onReceive(viewModel.$searchText) { (searchText) in
+                    viewModel.searchBusinesses()
+                }
+            Spacer()
             if viewModel.isLoading {
                 Spacer()
                 ProgressView()
@@ -32,11 +38,41 @@ struct FavoritesView: View {
 
 
 extension FavoritesView {
+    var searchBar: some View {
+        HStack {
+            TextField("Search ...", text: $viewModel.searchText)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    viewModel.isEditing = true
+                }
+                .onSubmit {
+                    viewModel.isEditing = false
+                    viewModel.searchBusinesses()
+                }
+
+            if viewModel.isEditing {
+                Button(action: {
+                    viewModel.searchText = ""
+                    viewModel.isEditing = false
+                    UIApplication.shared.endEditing()
+                }) {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                .animation(.default, value: 5)
+            }
+        }
+    }
     
     var businessList: some View {
         ScrollView {
             LazyVStack {
-                ForEach(viewModel.favoriteBusinesses) { business in
+                ForEach(viewModel.showFiltered ? viewModel.filteredBusinesses : viewModel.favoriteBusinesses) { business in
                     BusinessListView(configuration: viewModel.createBusinessListConfiguration(business))
                 }
             }
@@ -46,11 +82,17 @@ extension FavoritesView {
 
 extension FavoritesView {
     class ViewModel: ObservableObject {
+        @Published var searchText = ""
+        @Published var isEditing = false
         @Published var isLoading = false
         @Published var favoriteBusinesses = [BusinessModel]()
+        @Published var filteredBusinesses = [BusinessModel]()
 
+        var showFiltered: Bool {
+            !searchText.isEmpty
+        }
+        
         var dependencyContainer: DependencyContainer
-
 
         init(container: DependencyContainer) {
             self.dependencyContainer = container
@@ -62,6 +104,10 @@ extension FavoritesView {
 
         func viewDidDisappear(_ view: FavoritesView) {
             saveFavoriteBusinesses()
+        }
+        
+        func searchBusinesses() {
+            filteredBusinesses = favoriteBusinesses.filter{ $0.name.contains(searchText) || $0.categories[0].title.contains(searchText)}
         }
         
         func createBusinessListConfiguration(_ business: BusinessModel) -> BusinessListView.Configuration {
