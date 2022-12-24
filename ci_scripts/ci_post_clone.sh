8ef00b051c63806fe1f7d46c48350b5c514353c5
vspole <vishal.polepalli@gmail.com>
2022-12-24 17:33:53 -0500

    Updated post clone script location

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,12 +4,12 @@
 #  LetsGo
 #
 #  Created by Vishal Polepalli on 12/24/22.
-#  
+#
+
 cd ~/LetsGo
 touch GoogleService-Info.plist
-echo '${googlePLIST}' >> GoogleService-Info.plist
+echo $googlePLIST >> GoogleService-Info.plist
 
 cd ~/LetsGo/Secrets
 touch secrets.json
-echo '${secretsJSON}' >> secrets.json
-
+echo $secretsJSON >> secrets.json
