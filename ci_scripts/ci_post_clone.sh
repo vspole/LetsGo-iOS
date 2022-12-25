e51c3c823f240b6d4832365774f27393a40b35e8
vspole <vishal.polepalli@gmail.com>
2022-12-24 19:04:13 -0500

    Revert "Updated cd location in post clone script"
    This reverts commit 78bf550b8ec69a4283c222282fdfc62e4371f525.

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,10 +6,10 @@
 #  Created by Vishal Polepalli on 12/24/22.
 #
 
-cd /Volumes/workspace/repository/LetsGo
+cd ~/LetsGo
 touch GoogleService-Info.plist
 echo $googlePLIST >> GoogleService-Info.plist
 
-cd /Volumes/workspace/repository/LetsGo/Secrets
+cd ~/LetsGo/Secrets
 touch secrets.json
 echo $secretsJSON >> secrets.json
